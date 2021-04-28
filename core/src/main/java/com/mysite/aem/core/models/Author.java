aef036164d7a6cefa7457c03d2f10938b1195c98
Gopichand Reddy <62110262+Gopichand22@users.noreply.github.com>
2021-04-28 17:14:22 +0530

    fourth

--- core/src/main/java/com/mysite/aem/core/models/Author.java
+++ core/src/main/java/com/mysite/aem/core/models/Author.java
@@ -1,5 +1,40 @@
 package com.mysite.aem.core.models;
 
+import javax.inject.Inject;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+
+@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+
 public class Author {
+
+	@Inject
+	@Via("resource")
+	String name;
+
+	@Inject
+	@Via("resource")
+	int age;
+
+	@Inject
+	@Via("resource")
+	boolean check;
+
+	public String getFullName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public boolean getCheck() {
+		return check;
+	}
+	
+
 
 }
